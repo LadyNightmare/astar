68b204e70e7cfb35c53450d49b606e6c40003f6f
Manuel <32846375+M8anu@users.noreply.github.com>
2018-04-21 13:09:48 +0200

    V2 del RouteBot

--- src/searchPractice/RouteBot.java
+++ src/searchPractice/RouteBot.java
@@ -1,52 +1,94 @@
 package searchPractice;
 
-import Tools.BattleSpecs;
-import Tools.Coords;
-import robocode.Robot;
-
-import java.util.Random;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
-import static Tools.Coords.randomOriginalCoords;
+import Tools.Coords;
+import RobotBattleRunner.RBRunner;
+import robocode.Robot;
 
-public class RouteBot extends Robot {
-    private BattleSpecs specs;
-    private Coords[] duckCoords;
+public class RouteBot extends Robot
+{
 
-    @Override
-    public void run() {
-        setSpecs();
-        duckCoords = getDucks();
-        while (true) {
+  public void run ()
+  {
 
-            ahead(100);
+    Coords randomGenerator = new Coords(randomGenerator.getRow(), randomGenerator.getCol());
+    
+    randomGenerator.randomOriginalCoords(randomGenerator.getRow(), randomGenerator.getCol());
+    
+    List<Coords> list = AStarAlgorithm.Astar((seed2.getBattlefield()));
+    
+    System.out.println(list);
+    
+    if((int)getHeading()!=0)
+    {
+      turnLeft((int)getHeading());
+    }
+    if(list!=null)
+    {
+      while(list.size()>1)
+      {
+        Coords initialpos = new Coords(list.get(0).getRow(), list.get(0).getCol());
+        Coords nextpos = new Coords(list.get(1).getRow(), list.get(1).getCol());
 
-            turnGunRight(360);
+        if(nextpos.getCol() > initialpos.getCol()) {
+          if((int)getHeading()==0)
+          {
+            turnRight(90);
+          }else if((int)getHeading()==90)
+          {
+            turnRight(180);
+          }else if((int)getHeading()==180)
+          {
+            turnLeft(90);
+          }
+          ahead(64);
 
-            back(100);
+        } else if(nextpos.getCol() < initialpos.getCol())
+        {
+          if((int)getHeading()==0)
+          {
+            turnLeft(90);
+          }else if((int)getHeading()==180)
+          {
+            turnRight(90);
+          }else if((int)getHeading()==270)
+          {
+            turnRight(180);
+          }
+          ahead(64);
 
-            turnGunRight(360);
-            setSpecs();
+        } else if(nextpos.getRow() < initialpos.getRow())
+        {
+          if((int)getHeading()==90)
+          {
+            turnRight(90);
+          }else if((int)getHeading()==180)
+          {
+            turnRight(180);
+          }else if((int)getHeading()==270)
+          {
+            turnLeft(90);
+          }
+          ahead(64);
 
+        } else if(nextpos.getRow() > initialpos.getRow())
+        {
+          if((int)getHeading()==0)
+          {
+            turnLeft(180);
+          }else if((int)getHeading()==90)
+          {
+            turnLeft(90);
+          }else if((int)getHeading()==270)
+          {
+            turnRight(90);
+          }
+          ahead(64);
         }
+        list.remove(0);
+      }
     }
-
-    private void setSpecs() {
-        specs = new BattleSpecs();
-        specs.setNumRows((int) getBattleFieldHeight() / 64);
-        specs.setNumObstacles(getOthers());
-    }
-
-    private Coords[] getDucks() {
-        Coords duckCoords[] = new Coords[specs.getNumObstacles()];
-        Random seeder = ThreadLocalRandom.current();
-        seeder.setSeed(64);
-
-        for (int i = 0; i < specs.getNumObstacles(); i++) {
-            duckCoords[i] = randomOriginalCoords(specs, duckCoords, i, seeder);
-        }
-
-        return duckCoords;
-    }
-
+  }
 }
